75a85260e206a183d54b19871372760e596f2cb3
able.lv <able8@me.com>
2024-12-15 11:29:49 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
 # sched-slides-downloader
+
 Download slides from Sched for KubeCon, CloudNativeCon, IstioCon, GitOpsCon etc. 
+
+Inspired by https://github.com/denverdino/slides-downloader
+
+## How to use
+
+```bash
+go install github.com/able8/sched-slides-downloader
+
+sched-slides-downloader <event id> # See the following Events ID
+```
+
+## Events 
+
+https://colocatedeventsna2024.sched.com/
+
+* argocon2022
+* c2ds21
+* cdcon2020
+* cdcon2021
+* cloudnativedevxdayna21
+* cloudnativeebpfdayeu22
+* cloudnativeebpfdayna21
+* cloudnativeebpfdayna22
+* cloudnativerustdayeu21
+* cloudnativesecurityconeu22
+* cloudnativesecurityconna21
+* cloudnativesecurityconna22
+* cloudnativesecurityconna23
+* cloudnativesecurityconna24
+* cloudnativetelcodayna22
+* cloudnativewasmdayeu22
+* cloudnativewasmdayna21
+* cloudnativewasmdayna22
+* cloudnativewasmeu21
+* cnosvschina20cn
+* cnsdeu20
+* cnsdna20
+* cnsecuritydayeu21
+* colocatedeventseu2023
+* colocatedeventseu2024
+* colocatedeventsna2023
+* colocatedeventsna2024
+* crossplanedayeu21
+* dpdkchina2019
+* envoycon2019
+* envoycon2020
+* envoyconna21
+* fluentconeu21
+* gitopsconeu22
+* gitopsconna21
+* gitopsconna22
+* gitopssummit2021
+* hgf18
+* istiocon2023
+* istioconchina2023
+* kccna18
+* kccncchina2018chinese
+* kccnceu19
+* kccnceu20
+* kccnceu2021
+* kccnceu2022
+* kccnceu2023
+* kccnceu2024
+* kccncna19
+* kccncna20
+* kccncna2021
+* kccncna2022
+* kccncna2023
+* kccncna2024
+* kccncossaidevchn2024
+* kccncosschn19chi
+* kccncosschn19eng
+* kccncosschn2023
+* kccncosschn21
+* kcsna2019
+* knativeconeu22
+* knativeconna22
+* kubenetesaidayeu21
+* kubenetesedgedayeu21
+* kubernetesaidayeu22
+* kubernetesaidayna21
+* kubernetesaidayna22
+* kubernetesbatchdayeu22
+* kubernetesbatchdayna22
+* kubernetesonedgedayeu22
+* kubernetesonedgedayna22
+* magmadayeu21
+* onesummit2021
+* openobservabilitydayna22
+* ops2019
+* otcdna20
+* promconna21
+* promcononline2021
+* prometheusdayeu22
+* pytorch2024
+* servicemeshcon2019
+* servicemeshconeu20
+* servicemeshconeu21
+* servicemeshconeu22
+* servicemeshconna21
+* servicemeshconna22
+* smcna20
+* spiffespiredayna20
+* spseu20
+* spsna19
+* supplychainsecurityconna21
+* wasmcon2023
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide at least one event name as an argument.")
+	}
+
+	events := os.Args[1:] // Get all event names from command-line arguments
+
+	for _, event := range events {
+		downloadEventFiles(event)
+	}
+}
+
+// downloadEventFiles handles the crawling and downloading of event data
+func downloadEventFiles(event string) {
+	if err := os.MkdirAll(event, os.ModePerm); err != nil {
+		log.Fatalf("Failed to create data directory: %v", err)
+	}
+
+	collector := initializeCollector(event)
+
+	// Start crawling the overview page
+	startURL := fmt.Sprintf("https://%s.sched.com/overview", event)
+	log.Printf("Starting to crawl %s\n", startURL)
+
+	if err := collector.Visit(startURL); err != nil {
+		log.Printf("Failed to visit talk overview: %v", err)
+	}
+
+	fmt.Print("\n")
+}
+
+// initializeCollector initializes and configures the Colly collector
+func initializeCollector(event string) *colly.Collector {
+	c := colly.NewCollector(
+		colly.MaxBodySize(50 * 1024 * 1024), // Max retrieved response body is 50MB
+	)
+
+	// Set headers for requests
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:133.0) Gecko/20100101 Firefox/133.0")
+		r.Ctx.Put("event", event)
+	})
+
+	// Set rate limiting
+	if err := c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		Parallelism: 1,
+		RandomDelay: 2 * time.Second,
+	}); err != nil {
+		log.Printf("Failed setting rate limiting: %v", err)
+	}
+
+	// Define the first layer - overview links
+	c.OnHTML("div.list-simple div.sched-container-inner a", func(e *colly.HTMLElement) {
+		talkURL := fmt.Sprintf("https://%s.sched.com/%s", event, e.Attr("href"))
+		if err := e.Request.Visit(talkURL); err != nil {
+			log.Printf("Failed to visit talk URL: %v", err)
+		}
+	})
+
+	// Define the second layer - download links
+	c.OnHTML("a.file-uploaded", func(e *colly.HTMLElement) {
+		fileURL := e.Attr("href")
+		log.Printf("Found file in %s", fileURL)
+		// Visit the file URL to trigger the OnResponse callback
+		if err := e.Request.Visit(fileURL); err != nil {
+			log.Printf("Failed to visit file URL: %v", err)
+		}
+	})
+
+	// Handle the response for file downloads
+	c.OnResponse(func(r *colly.Response) {
+		if !strings.Contains(r.Request.URL.Path, "hosted_files") {
+			return
+		}
+
+		dirName := r.Request.Ctx.Get("event") // Retrieve the event name from the context
+		fileName := strings.ReplaceAll(r.FileName(), "hosted_files_", "")
+		filePath := fmt.Sprintf("%s/%s", dirName, fileName)
+		log.Printf("Downloading file %s from %s", filePath, r.Request.URL)
+
+		// Save the response body to a file
+		if err := r.Save(filePath); err != nil {
+			log.Printf("Failed to save file: %v", err)
+		} else {
+			log.Printf("Downloaded file: %s", filePath)
+		}
+	})
+
+	return c
+}
